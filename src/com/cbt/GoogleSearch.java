e39ef29e8c629267a11c874678bf7e4ab12b7a42
CybertekSchool <emreyildirim2018us@gmail.com>
2018-06-09 17:19:37 -0400

    add wooden spoon search from etsy.com automation

---
+++ src/com/cbt/GoogleSearch.java
@@ -0,0 +1,19 @@
+package com.cbt;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearch {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:/Users/Emre/Documents/selenium dependencies/drivers/chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.etsy.com/search?q=wooden%20spoon");
+
+	}
+
+}
